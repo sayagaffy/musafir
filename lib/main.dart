b2ba0a9d58476dbca666c18b40bdbb03d9afb166
sayagaffy <rgaffyagb@gmail.com>
2024-02-26 03:57:57 +0700

    test firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:musafir/firebase_options.dart';
 import 'package:musafir/ui/pages/get_started_page.dart';
 import 'package:musafir/ui/pages/main_page.dart';
 import 'package:musafir/ui/pages/sign_up_page.dart';
 import 'package:musafir/ui/pages/splash_widget.dart';
 import 'package:device_preview/device_preview.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(DevicePreview(
     enabled: true,
     builder: (BuildContext context) => const MainApp(),
