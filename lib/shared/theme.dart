4866668bab4d1f024874726c2e8d2b62d81ad02b
sayagaffy <rgaffyagb@gmail.com>
2024-03-14 23:21:40 +0700

    reconstruct const theme position

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -4,17 +4,17 @@ import 'package:google_fonts/google_fonts.dart';
 double defaultMargin = 18.0;
 double defaultRadius = 25.0;
 
-Color kPrimaryColor = const Color(0xffFDB82C);
-Color kBlackColor = const Color(0xff191D23);
-Color kWhiteColor = const Color(0xffFFFFFF);
-Color kGreyColor = const Color(0xff9698A9);
-Color kGreenColor = const Color(0xff0EC3AE);
-Color kRedColor = const Color(0xffEB70A5);
-Color kBackgroundColor = const Color(0xffFAFAFA);
-Color kInactiveColor = const Color(0xffDBD7EC);
-Color kTransparentColor = Colors.transparent;
-Color kAvailableColor = const Color(0xffE0D9FF);
-Color kUnavailableColor = const Color(0xffEBECF1);
+const Color kPrimaryColor = Color(0xffFDB82C);
+const Color kBlackColor = Color(0xff191D23);
+const Color kWhiteColor = Color(0xffFFFFFF);
+const Color kGreyColor = Color(0xff9698A9);
+const Color kGreenColor = Color(0xff0EC3AE);
+const Color kRedColor = Color(0xffEB70A5);
+const Color kBackgroundColor = Color(0xffFAFAFA);
+const Color kInactiveColor = Color(0xffDBD7EC);
+const Color kTransparentColor = Colors.transparent;
+const Color kAvailableColor = Color(0xffE0D9FF);
+const Color kUnavailableColor = Color(0xffEBECF1);
 
 TextStyle blackTextStyle = GoogleFonts.manrope(
   color: kBlackColor,
