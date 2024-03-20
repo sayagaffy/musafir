52b54ba5c13faea1fb761047649dab5ce857a680
RobertSoerbakti <robert.soerbakti@gmail.com>
2024-03-20 10:33:02 +0700

    #update icon

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/widgets.dart';
 
 import 'package:get/get.dart';
 import 'package:musafir/controllers/auth_controller.dart';
