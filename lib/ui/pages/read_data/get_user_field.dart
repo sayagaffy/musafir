43fa30b2aecab616840e371681fa29c141b88df3
sayagaffy <rgaffyagb@gmail.com>
2025-01-13 08:41:23 +0700

    doc explanation for get user flow

--- lib/ui/pages/read_data/get_user_field.dart
+++ lib/ui/pages/read_data/get_user_field.dart
@@ -1,3 +1,16 @@
+/// Widget GetUserField adalah widget stateless yang digunakan untuk mengambil
+/// dan menampilkan data dari dokumen pengguna di koleksi 'users' pada Firestore.
+///
+/// Parameter:
+/// - `documenId`: ID dokumen pengguna yang ingin diambil datanya.
+/// - `queryField`: Nama field yang ingin diambil dari dokumen pengguna.
+///
+/// Widget ini menggunakan FutureBuilder untuk mengambil data dari Firestore.
+/// Jika data berhasil diambil, widget akan menampilkan nilai dari field yang
+/// ditentukan. Jika data masih dalam proses pengambilan, widget akan menampilkan
+/// teks 'loading..'.
+library;
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
