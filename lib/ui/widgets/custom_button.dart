27094f931a66bd384c8ba6ec55a32d31f28bca88
sayagaffy <rgaffyagb@gmail.com>
2025-01-13 08:45:08 +0700

    add icon to custom button widget

--- lib/ui/widgets/custom_button.dart
+++ lib/ui/widgets/custom_button.dart
@@ -6,6 +6,7 @@ class CustomButton extends StatelessWidget {
   final double width;
   final Function() onPressed;
   final EdgeInsets margin;
+  final IconData? icon;
 
   const CustomButton({
     super.key,
@@ -13,6 +14,7 @@ class CustomButton extends StatelessWidget {
     this.width = double.infinity,
     required this.onPressed,
     this.margin = EdgeInsets.zero,
+    this.icon,
   });
 
   @override
@@ -29,14 +31,47 @@ class CustomButton extends StatelessWidget {
           ),
         ),
         onPressed: onPressed,
-        child: Text(
-          title,
-          style: whiteTextStyle.copyWith(
-            fontSize: 16,
-            fontWeight: semiBold,
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (icon != null) Icon(icon, color: Colors.white),
+            if (icon != null) const SizedBox(width: 8),
+            Text(
+              title,
+              style: whiteTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: width,
+//       height: 55,
+//       margin: margin,
+//       child: TextButton(
+//         style: TextButton.styleFrom(
+//           backgroundColor: kBlueColor,
+//           shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.circular(defaultRadius),
+//           ),
+//         ),
+//         onPressed: onPressed,
+//         child: Text(
+//           title,
+//           style: whiteTextStyle.copyWith(
+//             fontSize: 16,
+//             fontWeight: semiBold,
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
